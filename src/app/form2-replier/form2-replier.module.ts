c7dfd1cd28311ad7d0b06ee5cc238e53f0c47d62
a33573 <a33573@mail.cmuh.org.tw>
2023-02-01 14:54:28 +0800

    feat() 新增判斷devMode()

--- src/app/form2-replier/form2-replier.module.ts
+++ src/app/form2-replier/form2-replier.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -26,7 +26,7 @@ import { QrCodeModule } from 'ng-qrcode';
 // formio
 import { FormioModule, Templates } from '@formio/angular';
 import { Formio } from 'formiojs';
-(Formio as any).cdn.setBaseUrl('/web/cdn/formio')
+(Formio as any).cdn.setBaseUrl( isDevMode()? 'http://his-alpha.cmuh.org.tw/web/cdn/formio':'/web/cdn/formio')
 Templates.framework = 'bootstrap3';
 
 import { Form2PreLoadModule } from '../form2-pre-load/form2-pre-load.module';
